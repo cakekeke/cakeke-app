e88a793d190a473ce38f0b40ce8367ca31984674
mi-gongan <kib0202@naver.com>
2023-10-21 20:55:14 +0900

    ✨ feat: dotenv logic 추가

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,2 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 const baseUrl = 'http://43.202.0.192:8080/Cakeke';
-const naverMapClientId = 'YOUR_CLIENT_ID';
+String naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'] ?? "";
