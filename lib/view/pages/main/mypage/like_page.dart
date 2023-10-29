b2376d787aae65d671931b090e511cbdf971eec0
mi-gongan <kib0202@naver.com>
2023-10-29 22:14:39 +0900

    ♻️ refactor: mypage refact

--- lib/view/pages/main/mypage/like_page.dart
+++ lib/view/pages/main/mypage/like_page.dart
@@ -2,7 +2,7 @@ import 'package:cakeke/blocs/mypage/mypage_bloc.dart';
 import 'package:cakeke/blocs/mypage/mypage_event.dart';
 import 'package:cakeke/blocs/mypage/mypage_state.dart';
 import 'package:cakeke/view/widgets/common/scaffold_layout.dart';
-import 'package:cakeke/view/widgets/common/store_card.dart';
+import 'package:cakeke/view/widgets/main/mypage/like_store_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -28,40 +28,10 @@ class LikePage extends StatelessWidget {
             ),
             child: ListView(
               padding: const EdgeInsets.all(16),
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.08),
-                        offset: const Offset(0, 0),
-                        spreadRadius: 0,
-                        blurRadius: 8,
-                      ),
-                    ],
-                  ),
-                  child: StoreCard(),
-                ),
-                const SizedBox(height: 8),
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.08),
-                        offset: const Offset(0, 0),
-                        spreadRadius: 0,
-                        blurRadius: 8,
-                      ),
-                    ],
-                  ),
-                  child: StoreCard(),
-                ),
+              children: const [
+                LikeStoreCard(),
+                SizedBox(height: 8),
+                LikeStoreCard()
               ],
             ),
           ),
