37a931222b1c420fa87ef7103ffd702c34c55445
mi-gongan <kib0202@naver.com>
2024-02-11 22:35:12 +0900

    feat: webview loading

--- lib/view/pages/main/mypage/terms_page.dart
+++ lib/view/pages/main/mypage/terms_page.dart
@@ -1,32 +1,47 @@
 import 'package:cakeke/blocs/mypage/mypage_bloc.dart';
 import 'package:cakeke/blocs/mypage/mypage_event.dart';
 import 'package:cakeke/blocs/mypage/mypage_state.dart';
+import 'package:cakeke/config/design_system/design_system.dart';
 import 'package:cakeke/utils/webview.dart';
 import 'package:cakeke/view/widgets/common/scaffold_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class TermsPage extends StatelessWidget {
+class TermsPage extends StatefulWidget {
   TermsPage({super.key});
 
-  final controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..loadRequest(Uri.parse("${webviewUrl}terms"));
+  @override
+  State<TermsPage> createState() => _TermsPageState();
+}
+
+class _TermsPageState extends State<TermsPage> {
+  late WebViewController controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse("${webviewUrl}terms"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +52,24 @@ class TermsPage extends StatelessWidget {
           isDetailPage: true,
           onBackButtonPressed: () {
             context.read<MypageBloc>().add(MypagePageChanged(
-                  selectedPage: mypageTab.main.index,
+                  selectedPage: mypageTab.setting.index,
                 ));
           },
           backgroundColor: Colors.white,
           isSafeArea: true,
           bodyWidget: SafeArea(
-            child: WebViewWidget(
-              controller: controller,
+            child: Stack(
+              children: [
+                WebViewWidget(
+                  controller: controller,
+                ),
+                if (isLoading)
+                  Center(
+                    child: CircularProgressIndicator(
+                      color: DesignSystem.colors.appPrimary,
+                    ),
+                  ),
+              ],
             ),
           ),
         );
