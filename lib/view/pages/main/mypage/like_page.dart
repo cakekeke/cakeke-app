6627ae5b7ea45bc4eccf773e7e5589885ec7b020
mi-gongan <kib0202@naver.com>
2024-03-16 19:08:34 +0900

    fix: store card ui 통일

--- lib/view/pages/main/mypage/like_page.dart
+++ lib/view/pages/main/mypage/like_page.dart
@@ -1,8 +1,9 @@
 import 'package:cakeke/blocs/like/like_bloc.dart';
 import 'package:cakeke/blocs/mypage/mypage_bloc.dart';
 import 'package:cakeke/blocs/mypage/mypage_event.dart';
+import 'package:cakeke/blocs/store/store_bloc.dart';
 import 'package:cakeke/view/widgets/common/scaffold_layout.dart';
-import 'package:cakeke/view/widgets/main/mypage/like_store_card.dart';
+import 'package:cakeke/view/widgets/common/store_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -27,18 +28,29 @@ class LikePage extends StatelessWidget {
           isSafeArea: true,
           bodyWidget: storeList.isEmpty
               ? const EmptyListText()
-              : Container(
-                  decoration: const BoxDecoration(
-                    color: Color(0xffF4F4F4),
-                  ),
-                  child: ListView.builder(
-                    padding: const EdgeInsets.all(16),
-                    itemCount: storeList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final store = storeList.elementAt(index);
-                      return LikeStoreCard(store: store);
-                    },
-                  ),
+              : ListView.separated(
+                  itemCount: storeList.length,
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const Divider();
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: StoreCard(
+                        store: storeList[index],
+                        onTap: () {
+                          context.read<StoreBloc>().add(StoreEventStoreSelect(
+                                selectStore: storeList[index],
+                              ));
+                          context
+                              .read<MypageBloc>()
+                              .add(const MypagePageChanged(
+                                selectedPage: 3,
+                              ));
+                        },
+                      ),
+                    );
+                  },
                 ),
         );
       },
--- lib/view/widgets/common/store_card.dart
+++ lib/view/widgets/common/store_card.dart
@@ -14,70 +14,94 @@ class StoreCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: (store.imgUrlList.isEmpty
-                        ? const AssetImage("assets/images/splash.png")
-                        : NetworkImage(store.imgUrlList.first))
-                    as ImageProvider<Object>,
-                fit: BoxFit.cover,
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: (store.imgUrlList.isEmpty
+                          ? const AssetImage("assets/images/splash.png")
+                          : NetworkImage(store.imgUrlList.first))
+                      as ImageProvider<Object>,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      store.name,
-                      style: DesignSystem.typography.heading3(
-                          const TextStyle(fontWeight: FontWeight.w700)),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          store.name,
+                          style:
+                              DesignSystem.typography.heading3(const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            height: 1,
+                          )),
+                        ),
+                        LikeIconButton(store: store)
+                      ],
                     ),
-                    LikeIconButton(store: store)
-                  ],
-                ),
-                const SizedBox(height: 12),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 20,
-                      child: Text(
-                        '${store.starRating}',
-                        style: DesignSystem.typography.body2(),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                        child: Text(
+                          '${store.starRating}',
+                          style: DesignSystem.typography.body2(
+                            const TextStyle(
+                              fontWeight: FontWeight.w400,
+                              height: 1.4,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      ScoreWidget(score: store.starRating),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "${store.startTime} ~ ${store.endTime}",
+                    style: DesignSystem.typography.body2(
+                      const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        height: 1,
                       ),
                     ),
-                    const SizedBox(width: 4),
-                    ScoreWidget(score: store.starRating),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  "${store.startTime} ~ ${store.endTime}",
-                  style: DesignSystem.typography.body2(),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  store.roadAddress,
-                  overflow: TextOverflow.ellipsis,
-                  style: DesignSystem.typography.body2(),
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    store.roadAddress,
+                    overflow: TextOverflow.ellipsis,
+                    style: DesignSystem.typography.body2(
+                      const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        height: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
