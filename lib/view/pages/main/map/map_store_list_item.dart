f69f54f499271829e34d71953c3f0624b3af14ed
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2024-03-23 08:24:15 +0900

    feat : QA UI 개선 작업 진행

--- lib/view/pages/main/map/map_store_list_item.dart
+++ lib/view/pages/main/map/map_store_list_item.dart
@@ -27,25 +27,27 @@ class MapStoreListItem extends StatelessWidget {
                 selectedPage: 2,
               ));
         },
-        child: Container(
-            height: 170,
-            width: 90.w,
-            margin: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
-            padding: const EdgeInsets.all(12),
-            constraints: const BoxConstraints(
-              minHeight: 120,
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: DesignSystem.colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: DesignSystem.colors.black.withOpacity(0.33),
-                    blurRadius: 10,
-                  )
-                ]),
-            child: StoreCard(
-              store: store,
-            )));
+        child: SizedBox(
+          width: 100.w,
+          height: 170,
+          child: Container(
+              margin: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
+              padding: const EdgeInsets.all(12),
+              constraints: const BoxConstraints(
+                minHeight: 120,
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: DesignSystem.colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: DesignSystem.colors.black.withOpacity(0.33),
+                      blurRadius: 10,
+                    )
+                  ]),
+              child: StoreCard(
+                store: store,
+              )),
+        ));
   }
 }
