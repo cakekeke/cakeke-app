0dc8b2f33b9b4ba1d957f1dd8e409c364daf70b2
mi-gongan <kib0202@naver.com>
2024-05-23 01:18:30 +0900

    🐛 fix: api 주소 변경

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const baseUrl = 'http://52.79.120.153:8080/Cakeke';
+// const baseUrl = 'http://3.38.101.115:8080/Cakeke';
+const baseUrl = 'https://api.cakeke.shop/Cakeke';
 String naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'] ?? "";
