7b4d24b6b1eff3edcd7a819513a59133ea44dfd0
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2024-07-06 22:41:07 +0900

    fix : 심사 리젝 대응 및 기타 사항 업데이트

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const baseUrl = 'http://52.79.120.153:8080/Cakeke';
+const baseUrl = 'https://api.cakeke.shop/Cakeke';
 String naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'] ?? "";
