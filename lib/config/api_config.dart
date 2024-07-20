6e1ec3f8407894426be2c7bf1582b144bf6af9c8
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2024-07-20 09:22:03 +0900

    update : baseUrl 업데이트

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const baseUrl = 'https://api.cakeke.shop/Cakeke';
+const baseUrl = 'https://jeatest.store/Cakeke';
 String naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'] ?? "";
